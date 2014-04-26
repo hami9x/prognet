76527b4eafe96b82b431b4641a63e49ea017d612
phaikawl <phaikawl@gmail.com>
2014-04-26 12:25:15 +0700

    temp

---
+++ client/clientmain.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"path"
+
+	ng "github.com/gopherjs/go-angularjs"
+	"github.com/gopherjs/gopherjs/js"
+	"github.com/phaikawl/prognet/biz"
+)
+
+const (
+	ServerAddress = ""
+)
+
+var (
+	gLocalStorage *LocalStorage = InitLocalStorage()
+	gSiteData                   = &siteData{}
+)
+
+type userData struct {
+	Username string
+	Token    string
+}
+
+type siteData struct {
+	user *userData
+}
+
+func (d *siteData) User() *userData {
+	if d.user != nil {
+		return d.user
+	}
+	username, oku := gLocalStorage.Get("username")
+	token, okt := gLocalStorage.Get("token")
+	if !oku || !okt {
+		return nil
+	}
+	d.user = &userData{username, token}
+	return d.user
+}
+
+func (d *siteData) SetUser(user biz.User) {
+	d.user = &userData{user.Username, user.Token}
+	gLocalStorage.Set("username", user.Username)
+	gLocalStorage.Set("token", user.Token)
+}
+
+type LocalStorage struct {
+	js.Object
+}
+
+func InitLocalStorage() *LocalStorage {
+	return &LocalStorage{js.Global.Get("localStorage")}
+}
+
+func (ls *LocalStorage) Get(key string) (v string, ok bool) {
+	jsv := ls.Object.Call("getItem", key)
+	ok = !jsv.IsNull()
+	v = jsv.String()
+	return
+}
+
+func (ls *LocalStorage) Set(key string, value string) {
+	ls.Object.Call("setItem", key, value)
+}
+
+func serverUrl(p string) string {
+	return path.Join(ServerAddress, p)
+}
+
+func main() {
+	app := ng.NewModule("publicApp", []string{
+		"ngCookies",
+		"ngResource",
+		"ngSanitize",
+		"ngRoute",
+	})
+	app.NewController("HomeCtrl", func(scope *ng.Scope) {
+		scope.Set("user", gSiteData.User())
+	})
+	app.NewController("UserCtrl", func(scope *ng.Scope, http *ng.HttpService) {
+		http.Get(serverUrl("/auth")).Success(func(user biz.User, status int) {
+			gSiteData.SetUser(user)
+		})
+	})
+
+	app.Config(func(r *ng.RouteProvider) {
+		r.When("/", ng.RouteOptions(
+			ng.RouteController{"HomeCtrl"},
+			ng.RouteTemplate{"views/home.html"},
+		))
+		r.When("/user", ng.RouteOptions(
+			ng.RouteController{"UserCtrl"},
+			ng.RouteTemplate{"views/user.html"},
+		))
+	})
+
+	ng.AddHttpInterceptor(app, "authInterceptor", func(q *ng.QProvider, rootScope *ng.RootScope) ng.HttpInterceptor {
+		return ng.HttpInterceptor{
+			OnRequest: func(c *ng.ReqSpec) {
+				println(c)
+				token := gSiteData.User().Token
+				if token != "" {
+					c.Headers.Value["AuthToken"] = token
+				}
+				println("intercept it, yay!")
+			},
+			OnResponse: func(j js.Object) interface{} {
+				println(j)
+				return q.NowOrLater(j)
+			},
+		}
+	})
+}
--- main.go
+++ main.go
@@ -1,13 +1,98 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"html/template"
+	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/codegangsta/martini"
+	"github.com/coopernurse/gorp"
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/jmcvetta/randutil"
+	_ "github.com/lib/pq"
+	"github.com/phaikawl/prognet/biz"
 	"github.com/pilu/fresh/runner/runnerutils"
 )
+
+const (
+	mySigningKey = "n0t9r34t6cz9r34tn0t1na9r34tw4y"
+)
+
+var (
+	g *Environment = environment()
+)
+
+type Environment struct {
+	devMode bool
+	dbMap   *gorp.DbMap
+}
+
+func environment() *Environment {
+	return &Environment{
+		dbMap:   nil,
+		devMode: true,
+	}
+}
+
+func (g *Environment) IsDevMode() bool {
+	return g.devMode
+}
+
+func (g *Environment) InitDb() {
+	// connect to db using standard Go database/sql API
+	// use whatever database/sql driver you wish
+	db, err := sql.Open("postgres", "postgres://postgres:thebest1om@localhost:5432/prognet")
+	errPanic(err, "sql.Open failed")
+
+	// construct a gorp DbMap
+	g.dbMap = &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
+
+	g.dbMap.AddTableWithName(biz.Post{}, "posts").SetKeys(true, "Id")
+	g.dbMap.AddTableWithName(biz.User{}, "users").SetKeys(true, "Id")
+
+	// create the table. in a production system you'd generally
+	// use a migration tool, or create the tables via scripts
+	err = g.dbMap.CreateTablesIfNotExists()
+	errPanic(err, "Create tables failed")
+}
+
+func (g *Environment) Db() *gorp.DbMap {
+	if g.dbMap == nil {
+		panic("Trying to get an uninitialized dbmap")
+	}
+	return g.dbMap
+}
+
+func errPanic(err error, message string) {
+	if err != nil {
+		if message == "" {
+			message = err.Error()
+		}
+		log.Printf(message)
+		if g.IsDevMode() {
+			panic(err.Error())
+		}
+	}
+}
+
+func checkErr(err error) {
+	errPanic(err, "")
+}
+
+func makeRandomUserToken() (username string, tokenString string) {
+	token := jwt.New(jwt.GetSigningMethod("HS256"))
+	username, err := randutil.AlphaStringRange(5, 10)
+	errPanic(err, "Cannot random string, wtf?")
+	token.Claims["username"] = username
+	token.Claims["secret"] = time.Now().Add(time.Hour * 72).Unix()
+	tokenString, err = token.SignedString([]byte(mySigningKey))
+	errPanic(err, "Cannot sign string, wtf?")
+	return
+}
 
 func runnerMiddleware(w http.ResponseWriter, r *http.Request) {
 	if runnerutils.HasErrors() {
@@ -17,10 +102,15 @@ func runnerMiddleware(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	m := martini.Classic()
+	if os.Getenv("MARTINI_ENV") == "production" {
+		g.devMode = false
+	}
 
-	if os.Getenv("MARTINI_ENV") != "production" {
+	if g.IsDevMode() {
 		m.Use(runnerMiddleware)
 	}
+	g.InitDb()
+
 	m.Use(martini.Static("public/app"))
 
 	m.Get("/", func(r http.ResponseWriter) {
@@ -29,6 +119,22 @@ func main() {
 			panic(err.Error())
 		}
 		t.Execute(r, nil)
+	})
+
+	m.Get("/auth", func(r http.ResponseWriter) {
+		username, token := makeRandomUserToken()
+		user := &biz.User{
+			Username: username,
+			Token:    token,
+			Role:     biz.RoleUser,
+		}
+		checkErr(g.Db().Insert(user))
+		resp, err := json.Marshal(map[string]interface{}{
+			"username": user.Username,
+			"token":    user.Token,
+		})
+		checkErr(err)
+		r.Write(resp)
 	})
 	m.Run()
 }
